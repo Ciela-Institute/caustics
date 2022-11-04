fcd3519593d29118d317e83df4057911cb7621ae
Adam Coogan <dr.adam.coogan@gmail.com>
2022-11-04 17:33:36 -0400

    change to reduced alpha, get time delay working

--- src/caustic/lenses/sis.py
+++ src/caustic/lenses/sis.py
@@ -1,67 +1,25 @@
-from math import pi
-
-import torch
-
-from ..constants import G, arcsec_to_rad, c_km_s, c_Mpc_s, rad_to_arcsec
+from ..utils import transform_scalar_fn, transform_vector_fn
 from .base import AbstractLens
 
 
 class SIS(AbstractLens):
-    def __init__(self, thx0, thy0, sigma_v, cosmology=None, device=None):
-        """
-        Args:
-            sigma_v: velocity dispersion [km/s]
-        """
-        super().__init__(cosmology, device)
-        self.sigma_v = torch.as_tensor(sigma_v, dtype=torch.float32, device=device)
-        self.thx0 = torch.as_tensor(thx0, dtype=torch.float32, device=device)
-        self.thy0 = torch.as_tensor(thy0, dtype=torch.float32, device=device)
+    def __init__(self, thx0, thy0, th_ein, z_l, cosmology=None, device=None):
+        super().__init__(z_l, cosmology, device)
+        self.thx0 = thx0
+        self.thy0 = thy0
+        self.th_ein = th_ein
 
-    def xi_0(self, z_l, z_s):
-        d_l = self.cosmology.angular_diameter_dist(z_l)
-        d_s = self.cosmology.angular_diameter_dist(z_s)
-        d_ls = self.cosmology.angular_diameter_dist_z1z2(z_l, z_s)
-        return 4 * pi * (self.sigma_v / c_km_s)**2 * d_l * d_ls / d_s  # Mpc
-
-    def th_ein(self, z_l, z_s):
-        d_s = self.cosmology.angular_diameter_dist(z_s)
-        d_ls = self.cosmology.angular_diameter_dist_z1z2(z_l, z_s)
-        return 4 * pi * (self.sigma_v / c_km_s) ** 2 * d_ls / d_s * rad_to_arcsec
-
-    def Sigma(self, thx, thy, d_l):
-        thx = thx - self.thx0
-        thx = thx - self.thy0
-        xi = d_l * (thx**2 + thy**2).sqrt() * arcsec_to_rad
-        return self.sigma_v**2 / (2 * G * xi)
-
-    def Psi(self, thx, thy, z_l, z_s):
-        thx = thx - self.thx0
-        thy = thy - self.thy0
-        return (thx**2 + thy**2).sqrt()  # arcsec
-
-    def Psi_hat(self, thx, thy, z_l, z_s):
-        d_l = self.cosmology.angular_diameter_dist(z_l)
-        return self.Psi(thx, thy, z_l, z_s) * (self.xi_0(z_l, z_s) / d_l)**2  # arcsec
-
-    def alpha(self, thx, thy, z_l, z_s):
-        thx = thx - self.thx0
-        thy = thy - self.thy0
+    @transform_vector_fn
+    def alpha(self, thx, thy, z_s):
         th = (thx**2 + thy**2).sqrt()
-        return thx / th, thy / th  # arcsec
+        return self.th_ein * thx / th, self.th_ein * thy / th
 
-    def alpha_hat(self, thx, thy, z_l, z_s):
-        d_l = self.cosmology.angular_diameter_dist(z_l)
-        d_s = self.cosmology.angular_diameter_dist(z_s)
-        d_ls = self.cosmology.angular_diameter_dist_z1z2(z_l, z_s)
-        factor = self.xi_0(z_l, z_s) * d_s / d_l / d_ls
-        ax, ay = self.alpha(thx, thy, z_l, z_s)
-        return factor * ax, factor * ay  # arcsec
+    @transform_scalar_fn
+    def Psi(self, thx, thy, z_s):
+        th = (thx**2 + thy**2).sqrt()
+        return self.th_ein * th
 
-    # def time_delay(self, thx, thy, z_l, z_s):
-    #     d_l = self.cosmology.angular_diameter_dist(z_l)
-    #     d_s = self.cosmology.angular_diameter_dist(z_s)
-    #     d_ls = self.cosmology.angular_diameter_dist_z1z2(z_l, z_s)
-    #     factor = (1 + z_l) / c_Mpc_s * d_s * self.xi_0(z_l, z_s)**2 / d_l / d_ls
-    #     ax, ay = self.alpha(thx, thy, z_l, z_s)
-    #     fp_hat = 0.5 * (ax**2 + ay**2) - self.Psi(thx, thy, z_l, z_s)
-    #     return factor * fp_hat * arcsec_to_rad**2
+    @transform_scalar_fn
+    def kappa(self, thx, thy, z_s):
+        th = (thx**2 + thy**2).sqrt()
+        return self.th_ein / th
--- src/caustic/utils.py
+++ src/caustic/utils.py
@@ -36,37 +36,39 @@ def translate_rotate(
         return x, y
 
 
-def transform_scalar_fn(fn):
+def transform_scalar_fn(fn, x_name="thx0", y_name="thy0", phi_name="phi"):
     @wraps(fn)
     def wrapped(self, x, y, *args, **kwargs):
-        xt = x - self.x_0
-        yt = y - self.y_0
-
-        if hasattr(self, "phi"):
-            c_phi = self.phi.cos()
-            s_phi = self.phi.sin()
-            xt = x * c_phi - y * s_phi
-            yt = x * s_phi + y * c_phi
+        xt = x - getattr(self, x_name)
+        yt = y - getattr(self, y_name)
+        if hasattr(self, phi_name):
+            # Apply R(-phi)
+            phi = getattr(self, phi_name)
+            c_phi = phi.cos()
+            s_phi = phi.sin()
+            xt = x * c_phi + y * s_phi
+            yt = -x * s_phi + y * c_phi
+            # Evaluate function
 
         return fn(self, xt, yt, *args, **kwargs)
 
     return wrapped
 
 
-def transform_vector_fn(fn):
+def transform_vector_fn(fn, x_name="thx0", y_name="thy0", phi_name="phi"):
     @wraps(fn)
     def wrapped(self, x, y, *args, **kwargs):
-        xt = x - self.x_0
-        yt = y - self.y_0
-        if hasattr(self, "phi"):
+        xt = x - getattr(self, x_name)
+        yt = y - getattr(self, y_name)
+        if hasattr(self, phi_name):
             # Apply R(-phi)
-            c_phi = self.phi.cos()
-            s_phi = self.phi.sin()
+            phi = getattr(self, phi_name)
+            c_phi = phi.cos()
+            s_phi = phi.sin()
             xt = x * c_phi + y * s_phi
             yt = -x * s_phi + y * c_phi
-
+            # Evaluate function
             vx, vy = fn(self, xt, yt, *args, **kwargs)
-
             # Apply R(phi)
             return vx * c_phi - vy * s_phi, vx * s_phi + vy * c_phi
         else:
@@ -80,10 +82,10 @@ def to_elliptical(x, y, q):
     """
     Converts to elliptical Cartesian coordinates.
     """
-    return x * q.sqrt(), y / q.sqrt()
+    return x * q, y
 
 
 def get_meshgrid(resolution, nx, ny, device=None) -> Tuple[Tensor, Tensor]:
     xs = torch.linspace(-1, 1, nx, device=device) * resolution * (nx - 1) / 2
     ys = torch.linspace(-1, 1, ny, device=device) * resolution * (ny - 1) / 2
-    return torch.meshgrid((xs, ys), indexing="xy")
+    return torch.meshgrid([xs, ys], indexing="xy")
