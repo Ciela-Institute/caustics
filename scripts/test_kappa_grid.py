c50e9ea0c5147dcbe79f12114904e9dc84bdbf06
Adam Coogan <dr.adam.coogan@gmail.com>
2022-11-07 14:32:47 -0500

    update kappa grid test script

--- scripts/test_kappa_grid.py
+++ scripts/test_kappa_grid.py
@@ -9,13 +9,13 @@ from caustic.utils import get_meshgrid
 
 device = "cpu"
 dtype = torch.float64
-kappa_mode = "fft"
+kappa_mode = "conv2d"
 
 cosmology = FlatLambdaCDMCosmology()
 res = 0.05
-nx = ny = 100
-fov = res * nx
-thx, thy = get_meshgrid(res, nx, ny, device=device, dtype=dtype)
+n_pix = 100
+fov = res * n_pix
+thx, thy = get_meshgrid(res, n_pix, n_pix, device=device, dtype=dtype)
 
 z_l = torch.tensor(1.0, device=device, dtype=dtype)
 z_s = torch.tensor(2.5, device=device, dtype=dtype)
@@ -26,8 +26,6 @@ sis = SIS(thx0, thy0, th_ein, z_l, cosmology, device)
 
 kappa_sis = KappaGrid(
     sis.kappa(thx, thy, z_s)[None, None, :, :],
-    0.0,
-    0.0,
     fov,
     kappa_mode,
     dtype,
@@ -47,11 +45,11 @@ plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
 ax.set_title(r"$\log_{10} \kappa$")
 alpha_x, alpha_y = sis.alpha(thx, thy, z_s)
 ax = axes[0, 1]
-im = ax.imshow(alpha_x.squeeze().cpu(), origin="lower")
+im = ax.imshow(alpha_x.squeeze().cpu(), origin="lower", cmap="bwr")
 plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
 ax.set_title(r"$\alpha_x$")
 ax = axes[0, 2]
-im = ax.imshow(alpha_y.squeeze().cpu(), origin="lower")
+im = ax.imshow(alpha_y.squeeze().cpu(), origin="lower", cmap="bwr")
 plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
 ax.set_title(r"$\alpha_y$")
 
@@ -60,12 +58,12 @@ im = ax.imshow(kappa_sis._kappa.log().squeeze().cpu(), origin="lower")
 plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
 alpha_x, alpha_y = kappa_sis._method()
 ax = axes[1, 1]
-im = ax.imshow(alpha_x.squeeze().cpu(), origin="lower")
+im = ax.imshow(alpha_x.squeeze().cpu(), origin="lower", cmap="bwr")
 plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
 ax = axes[1, 2]
-im = ax.imshow(alpha_y.squeeze().cpu(), origin="lower")
+im = ax.imshow(alpha_y.squeeze().cpu(), origin="lower", cmap="bwr")
 plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
 
 fig.tight_layout()
-fig.suptitle(f"{kappa_mode} {dtype}")
-plt.show()
+fig.suptitle(f"{n_pix} pixels, {kappa_mode}, {dtype}")
+plt.savefig(f"/Users/amcoogan/Downloads/figures/{n_pix}_pixels_{kappa_mode}_{dtype}.png")
