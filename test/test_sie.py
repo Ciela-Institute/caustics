91dc2c4f3780f6c8b3e63ba4a47e27d7c623685c
Adam Coogan <dr.adam.coogan@gmail.com>
2022-12-09 11:48:37 -0500

    refactor tests

--- test/test_sie.py
+++ test/test_sie.py
@@ -1,86 +1,39 @@
 from math import pi
 
 import lenstronomy.Util.param_util as param_util
-import numpy as np
 import torch
-from lenstronomy.Data.pixel_grid import PixelGrid
 from lenstronomy.LensModel.lens_model import LensModel
+from utils import lens_test_helper
 
 from caustic.lenses import SIE
-from caustic.utils import get_meshgrid
 
 
-def setup():
-    # Caustic setup
-    res = 0.05
-    nx = 200
-    ny = 200
-    thx, thy = get_meshgrid(res, nx, ny)
-    sie = SIE()
+def test():
+    atol = 1e-5
+    rtol = 1e-5
 
-    # Lenstronomy setup
-    ra_at_xy_0, dec_at_xy_0 = (-5 + res / 2, -5 + res / 2)
-    transform_pix2angle = np.array([[1, 0], [0, 1]]) * res
-    kwargs_pixel = {
-        "nx": nx,
-        "ny": ny,  # number of pixels per axis
-        "ra_at_xy_0": ra_at_xy_0,
-        "dec_at_xy_0": dec_at_xy_0,
-        "transform_pix2angle": transform_pix2angle,
-    }
-    pixel_grid = PixelGrid(**kwargs_pixel)
-    thx_ls, thy_ls = pixel_grid.coordinate_grid(nx, ny)
+    # Models
+    lens = SIE()
     lens_model_list = ["SIE"]
-    sie_ls = LensModel(lens_model_list=lens_model_list)
+    lens_ls = LensModel(lens_model_list=lens_model_list)
 
     # Parameters
-    thx0_sie = torch.tensor(0.912)
-    thy0_sie = torch.tensor(-0.442)
-    q_sie = torch.tensor(0.7)
-    phi_sie = torch.tensor(pi / 3)
-    b_sie = torch.tensor(1.4)
-    s_sie = torch.tensor(0.0)
-    args = (thx0_sie, thy0_sie, q_sie, phi_sie, b_sie, s_sie)
-    e1, e2 = param_util.phi_q2_ellipticity(phi=phi_sie.item(), q=q_sie.item())
+    thx0 = torch.tensor(0.912)
+    thy0 = torch.tensor(-0.442)
+    q = torch.tensor(0.7)
+    phi = torch.tensor(pi / 3)
+    b = torch.tensor(1.4)
+    s = torch.tensor(0.0)
+    args = (None, None, None, thx0, thy0, q, phi, b, s)
+    e1, e2 = param_util.phi_q2_ellipticity(phi=phi.item(), q=q.item())
     kwargs_ls = [
         {
-            "theta_E": b_sie.item(),
+            "theta_E": b.item(),
             "e1": e1,
             "e2": e2,
-            "center_x": thx0_sie.item(),
-            "center_y": thy0_sie.item(),
+            "center_x": thx0.item(),
+            "center_y": thy0.item(),
         }
     ]
 
-    return sie, sie_ls, args, kwargs_ls, thx, thy, thx_ls, thy_ls
-
-
-def test_alpha():
-    sie, sie_ls, args, kwargs_ls, thx, thy, thx_ls, thy_ls = setup()
-
-    # Cosmologies and redshifts don't matter since SIE is not a physical model
-    alpha_x, alpha_y = sie.alpha(thx, thy, None, None, None, *args)
-    alpha_x_ls, alpha_y_ls = sie_ls.alpha(thx_ls, thy_ls, kwargs_ls)
-
-    assert np.allclose(alpha_x.numpy(), alpha_x_ls, atol=1e-5)
-    assert np.allclose(alpha_y.numpy(), alpha_y_ls, atol=1e-5)
-
-
-def test_Psi():
-    sie, sie_ls, args, kwargs_ls, thx, thy, thx_ls, thy_ls = setup()
-
-    # Cosmologies and redshifts don't matter since SIE is not a physical model
-    Psi = sie.Psi(thx, thy, None, None, None, *args)
-    Psi_ls = sie_ls.potential(thx_ls, thy_ls, kwargs_ls)
-
-    assert np.allclose(Psi.numpy(), Psi_ls)
-
-
-def test_kappa():
-    sie, sie_ls, args, kwargs_ls, thx, thy, thx_ls, thy_ls = setup()
-
-    # Cosmologies and redshifts don't matter since SIE is not a physical model
-    kappa = sie.kappa(thx, thy, None, None, None, *args)
-    kappa_ls = sie_ls.kappa(thx_ls, thy_ls, kwargs_ls)
-
-    assert np.allclose(kappa.numpy(), kappa_ls, atol=1e-5)
+    lens_test_helper(lens, lens_ls, args, kwargs_ls, rtol, atol)
---
+++ test/utils.py
@@ -0,0 +1,65 @@
+from typing import Any, Dict, List, Tuple
+
+import numpy as np
+from lenstronomy.Data.pixel_grid import PixelGrid
+from lenstronomy.LensModel.lens_model import LensModel
+
+from caustic.lenses import AbstractLens
+from caustic.utils import get_meshgrid
+
+
+def setup_grids(res=0.05, n_pix=200):
+    # Caustic setup
+    thx, thy = get_meshgrid(res, n_pix, n_pix)
+
+    # Lenstronomy setup
+    ra_at_xy_0, dec_at_xy_0 = (-5 + res / 2, -5 + res / 2)
+    transform_pix2angle = np.array([[1, 0], [0, 1]]) * res
+    kwargs_pixel = {
+        "nx": n_pix,
+        "ny": n_pix,  # number of pixels per axis
+        "ra_at_xy_0": ra_at_xy_0,
+        "dec_at_xy_0": dec_at_xy_0,
+        "transform_pix2angle": transform_pix2angle,
+    }
+    pixel_grid = PixelGrid(**kwargs_pixel)
+    thx_ls, thy_ls = pixel_grid.coordinate_grid(n_pix, n_pix)
+    return thx, thy, thx_ls, thy_ls
+
+
+def alpha_test_helper(lens, lens_ls, args, kwargs_ls, atol, rtol):
+    thx, thy, thx_ls, thy_ls = setup_grids()
+    # Cosmologies and redshifts don't matter since SIE is not a physical model
+    alpha_x, alpha_y = lens.alpha(thx, thy, *args)
+    alpha_x_ls, alpha_y_ls = lens_ls.alpha(thx_ls, thy_ls, kwargs_ls)
+    assert np.allclose(alpha_x.numpy(), alpha_x_ls, rtol, atol)
+    assert np.allclose(alpha_y.numpy(), alpha_y_ls, rtol, atol)
+
+
+def Psi_test_helper(lens, lens_ls, args, kwargs_ls, atol, rtol):
+    thx, thy, thx_ls, thy_ls = setup_grids()
+    # Cosmologies and redshifts don't matter since SIE is not a physical model
+    Psi = lens.Psi(thx, thy, *args)
+    Psi_ls = lens_ls.potential(thx_ls, thy_ls, kwargs_ls)
+    assert np.allclose(Psi.numpy(), Psi_ls, rtol, atol)
+
+
+def kappa_test_helper(lens, lens_ls, args, kwargs_ls, atol, rtol):
+    thx, thy, thx_ls, thy_ls = setup_grids()
+    # Cosmologies and redshifts don't matter since SIE is not a physical model
+    kappa = lens.kappa(thx, thy, *args)
+    kappa_ls = lens_ls.kappa(thx_ls, thy_ls, kwargs_ls)
+    assert np.allclose(kappa.numpy(), kappa_ls, rtol, atol)
+
+
+def lens_test_helper(
+    lens: AbstractLens,
+    lens_ls: LensModel,
+    args: Tuple[Any, ...],
+    kwargs_ls: List[Dict[str, Any]],
+    rtol,
+    atol,
+):
+    alpha_test_helper(lens, lens_ls, args, kwargs_ls, atol, rtol)
+    Psi_test_helper(lens, lens_ls, args, kwargs_ls, atol, rtol)
+    kappa_test_helper(lens, lens_ls, args, kwargs_ls, atol, rtol)
