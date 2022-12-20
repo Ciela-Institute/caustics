23013e3bfbd420fbf73e5bf13ba42b719fc9e236
Andreas Filipp <94164628+andreasfilipp@users.noreply.github.com>
2022-12-20 16:32:29 +0100

    Update epl.py
    changed function alpha and added function _r_omega to work around the hypergeometric function form scipy.special

--- src/caustic/lenses/epl.py
+++ src/caustic/lenses/epl.py
@@ -19,17 +19,46 @@ class EPL(AbstractThinLens):
         """
         Args:
             b: scale length.
-            t: power law slope.
+            t: power law slope.  (I believe for us t = gamma-1?)
             s: core radius.
         """
         s = torch.tensor(0.0, device=self.device, dtype=thx0.dtype) if s is None else s
         thx, thy = translate_rotate(thx, thy, thx0, thy0, phi)
+        psi = self._get_psi(thx, thy, q, s)
+        
+        # follow Tessore et al 2015 (eq. 5)
+        z = q * thx + thy * 1j
+        r = torch.abs(z)
+        
+        # Tessore et al 2015 (eq. 23)
+        alpha_c = 2. / (1. + q) * (b / r)**t * self._r_omega(z, t, q, n=25) #not sure if we want the n higher/lower or costumizable
+        
+        alpha_real = torch.nan_to_num(alpha_c.real, posinf=(1e12), neginf=(-1e12))
+        alpha_imag = torch.nan_to_num(alpha_c.imag, posinf=(1e12), neginf=(-1e12))
+        return derotate(alpha_real, alpha_imag, psi)
 
-        # TODO: fill in code to compute the deflection angles ax and ay
-        ax = ...
-        ay = ...
-
-        return derotate(ax, ay, phi)
+    def _r_omega(self, z, t, q, n):
+        '''
+        z = R * e^(i * phi):    position vector in the lens plane (complex torch tensor)
+        t = gamma - 1:          mass slope of the profile
+        q:                      axis ratio
+        n:                      number of iterations
+        This iterative implementation of the hypergeometric function `hyp2f1` in `scipy.special`
+        Note: the value returned includes an extra factor R multiplying eq. (23) for omega(phi).
+        '''
+        # constants
+        f = (1. - q)/(1. + q)
+        phi = z/torch.conj(z)
+        
+        # first term in series
+        omega_i = z 
+        part_sum = omega_i
+        
+        for i in range(1, n):
+            factor = (2. * i - (2. - t)) / (2. * i + (2. - t))
+            omega_i = -f * factor * phi * omega_i
+            part_sum += omega_i
+        return part_sum
 
     def Psi(self, thx, thy, z_l, z_s, cosmology, thx0, thy0, q, phi, b, t, s=None):
         thx, thy = translate_rotate(thx, thy, thx0, thy0, phi)
