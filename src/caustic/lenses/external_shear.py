93c36b3bebfdbfed8773eb3a671be1166ccc40c8
Adam Coogan <dr.adam.coogan@gmail.com>
2022-12-20 13:52:29 -0500

    fix shear Psi bug

--- src/caustic/lenses/external_shear.py
+++ src/caustic/lenses/external_shear.py
@@ -13,17 +13,12 @@ class ExternalShear(AbstractThinLens):
         # Meneghetti eq 3.83
         a1 = thx * gamma_1 + thy * gamma_2
         a2 = thx * gamma_2 - thy * gamma_1
-        return a1, a2  #I'm not sure but I think no derotation necessary
+        return a1, a2  # I'm not sure but I think no derotation necessary
 
     def Psi(self, thx, thy, z_l, z_s, cosmology, thx0, thy0, gamma_1, gamma_2):
         thx, thy = translate_rotate(thx, thy, thx0, thy0)
         ax, ay = self.alpha(thx, thy, z_l, z_s, cosmology, 0.0, 0.0, gamma_1, gamma_2)
-        return thx * ax + thy * ay
+        return 0.5 * (thx * ax + thy * ay)
 
     def kappa(self, thx, thy, z_l, z_s, cosmology, thx0, thy0, gamma_1, gamma_2):
         raise ValueError("convergence undefined for external shear")
-        
-    def external_shear_potential(self, thx, thy, z_l, z_s, cosmology, thx0, thy0, gamma_1, gamma_2):
-        #not sure if we need this fct - I added it
-        thx, thy = translate_rotate(thx, thy, thx0, thy0)
-        return 0.5 * (gamma_1 * thx**2. + 2 * gamma_2 * thx * thy - gamma_1 * thy**2.)
