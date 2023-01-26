9a2604e14cb0b40afd2c08921fd4717da4cc049d
Andreas Filipp <94164628+andreasfilipp@users.noreply.github.com>
2023-01-26 10:25:32 -0500

    Update test_nfw.py
    update test_nfw

--- test/test_nfw.py
+++ test/test_nfw.py
@@ -1,6 +1,6 @@
-from math import pi
+#from math import pi
 
-import lenstronomy.Util.param_util as param_util
+#import lenstronomy.Util.param_util as param_util
 import torch
 from lenstronomy.LensModel.lens_model import LensModel
 from utils import lens_test_helper
@@ -8,6 +8,9 @@ from utils import lens_test_helper
 from caustic.cosmology import FlatLambdaCDMCosmology
 from caustic.lenses import NFW
 
+#next to imports to get Rs_angle and alpha_Rs in arcsec for lenstronomy
+from lenstronomy.Cosmo.lens_cosmo import LensCosmo
+from astropy.cosmology import FlatLambdaCDM
 
 def test():
     atol = 1e-5
@@ -27,18 +30,19 @@ def test():
     m = torch.tensor(1e12)
     c = torch.tensor(15.0)
     args = (z_l, z_s, cosmology, thx0, thy0, m, c)
-    # TODO: parse parameters into form required by lenstronomy
-    # See https://lenstronomy.readthedocs.io/en/latest/lenstronomy.LensModel.Profiles.html#lenstronomy.LensModel.Profiles.nfw.NFW
-    # e1, e2 = param_util.phi_q2_ellipticity(phi=phi.item(), q=q.item())
-    # kwargs_ls = [
-    #     {
-    #         "theta_E": b.item(),
-    #         "e1": e1,
-    #         "e2": e2,
-    #         "center_x": thx0.item(),
-    #         "center_y": thy0.item(),
-    #     }
-    # ]
+    
+    cosmo = FlatLambdaCDM(H0=70, Om0=0.3, Ob0=0.05)
+    lens_cosmo = LensCosmo(z_lens=z_l.item(), z_source=z_s.item(), cosmo=cosmo)
+    Rs_angle, alpha_Rs = lens_cosmo.nfw_physical2angle(M=m.item(), c=c.item())
+    
+    #lenstronomy params ['Rs', 'alpha_Rs', 'center_x', 'center_y']
+    kwargs_ls = [
+        {
+            "Rs": Rs_angle,
+            "alpha_Rs": alpha_Rs,
+            "center_x": thx0.item(),
+            "center_y": thy0.item(),
+        }
+    ]
 
-    # TODO: uncomment
-    # lens_test_helper(lens, lens_ls, args, kwargs_ls, rtol, atol)
+    lens_test_helper(lens, lens_ls, args, kwargs_ls, rtol, atol)
