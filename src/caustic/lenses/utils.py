1cbb54a8777bc801aad8969b26d9cfcd68ba5f22
Connor Stone <connorstone628@gmail.com>
2023-05-12 10:28:28 -0400

    finishing off lenses and add documentation for sources

--- src/caustic/lenses/utils.py
+++ src/caustic/lenses/utils.py
@@ -11,12 +11,21 @@ __all__ = ("get_pix_jacobian", "get_pix_magnification", "get_magnification")
 def get_pix_jacobian(
     raytrace, thx, thy, z_s, x
 ) -> Tuple[Tuple[Tensor, Tensor], Tuple[Tensor, Tensor]]:
-    """
-    Jacobian :math:`\\partial \beta / \\partial \theta` at a single point.
+    """Computes the Jacobian matrix of the partial derivatives of the
+    image position with respect to the source position
+    (:math:`\\partial \beta / \\partial \theta`).  This is done at a
+    single point on the lensing plane.
 
-    Note:
-        Currently uses `jacrev` due to upcasting bug with `jacfwd` (https://github.com/pytorch/pytorch/issues/90065).
-        Should probably switch to `jacfwd` in the future.
+    Args:
+        raytrace: A function that maps the lensing plane coordinates to the source plane coordinates.
+        thx (Tensor): The x-coordinate on the lensing plane.
+        thy (Tensor): The y-coordinate on the lensing plane.
+        z_s (Tensor): The redshift of the source.
+        x: Additional parameters for the raytrace function.
+
+    Returns:
+        The Jacobian matrix of the image position with respect to the source position at the given point.
+
     """
     jac = torch.func.jacfwd(raytrace, (0, 1))(thx, thy, z_s, x)  # type: ignore
     return jac
@@ -24,7 +33,18 @@ def get_pix_jacobian(
 
 def get_pix_magnification(raytrace, thx, thy, z_s, x) -> Tensor:
     """
-    Magnification at a single point.
+    Computes the magnification at a single point on the lensing plane. The magnification is derived from the determinant
+    of the Jacobian matrix of the image position with respect to the source position.
+
+    Args:
+        raytrace: A function that maps the lensing plane coordinates to the source plane coordinates.
+        thx (Tensor): The x-coordinate on the lensing plane.
+        thy (Tensor): The y-coordinate on the lensing plane.
+        z_s (Tensor): The redshift of the source.
+        x: Additional parameters for the raytrace function.
+
+    Returns:
+        The magnification at the given point on the lensing plane.
     """
     jac = get_pix_jacobian(raytrace, thx, thy, z_s, x)
     return 1 / (jac[0][0] * jac[1][1] - jac[0][1] * jac[1][0]).abs()
@@ -32,7 +52,18 @@ def get_pix_magnification(raytrace, thx, thy, z_s, x) -> Tensor:
 
 def get_magnification(raytrace, thx, thy, z_s, x) -> Tensor:
     """
-    Magnification over a grid.
+    Computes the magnification over a grid on the lensing plane. This is done by calling `get_pix_magnification` 
+    for each point on the grid.
+
+    Args:
+        raytrace: A function that maps the lensing plane coordinates to the source plane coordinates.
+        thx (Tensor): The x-coordinates on the lensing plane.
+        thy (Tensor): The y-coordinates on the lensing plane.
+        z_s (Tensor): The redshift of the source.
+        x: Additional parameters for the raytrace function.
+
+    Returns:
+        A tensor representing the magnification at each point on the grid.
     """
     return vmap_n(get_pix_magnification, 2, (None, 0, 0, None, None))(
         raytrace, thx, thy, z_s, x
--- src/caustic/sources/sersic.py
+++ src/caustic/sources/sersic.py
@@ -10,6 +10,24 @@ __all__ = ("Sersic",)
 
 
 class Sersic(Source):
+    """
+    `Sersic` is a subclass of the abstract class `Source`. It represents a source in a strong 
+    gravitational lensing system that follows a Sersic profile, a mathematical function that describes 
+    how the intensity I of a galaxy varies with distance r from its center.
+    
+    The Sersic profile is often used to describe elliptical galaxies and spiral galaxies' bulges.
+
+    Attributes:
+        thx0 (Optional[Tensor]): The x-coordinate of the Sersic source's center. 
+        thy0 (Optional[Tensor]): The y-coordinate of the Sersic source's center.
+        q (Optional[Tensor]): The axis ratio of the Sersic source.
+        phi (Optional[Tensor]): The orientation of the Sersic source.
+        index (Optional[Tensor]): The Sersic index, which describes the degree of concentration of the source.
+        th_e (Optional[Tensor]): The scale length of the Sersic source.
+        I_e (Optional[Tensor]): The intensity at the effective radius.
+        s (float): A small constant for numerical stability.
+        lenstronomy_k_mode (bool): A flag indicating whether to use lenstronomy to compute the value of k.
+    """
     def __init__(
         self,
         name: str,
@@ -23,6 +41,21 @@ class Sersic(Source):
         s: float = 0.0,
         use_lenstronomy_k=False,
     ):
+        """
+        Constructs the `Sersic` object with the given parameters. 
+
+        Args:
+            name (str): The name of the source.
+            thx0 (Optional[Tensor]): The x-coordinate of the Sersic source's center.
+            thy0 (Optional[Tensor]): The y-coordinate of the Sersic source's center.
+            q (Optional[Tensor]): The axis ratio of the Sersic source.
+            phi (Optional[Tensor]): The orientation of the Sersic source.
+            index (Optional[Tensor]): The Sersic index, which describes the degree of concentration of the source.
+            th_e (Optional[Tensor]): The scale length of the Sersic source.
+            I_e (Optional[Tensor]): The intensity at the effective radius.
+            s (float): A small constant for numerical stability.
+            use_lenstronomy_k (bool): A flag indicating whether to use lenstronomy to compute the value of k.
+        """
         super().__init__(name)
         self.add_param("thx0", thx0)
         self.add_param("thy0", thy0)
@@ -36,6 +69,30 @@ class Sersic(Source):
         self.lenstronomy_k_mode = use_lenstronomy_k
 
     def brightness(self, thx, thy, x):
+        """
+        Implements the `brightness` method for `Sersic`. The brightness at a given point is 
+        determined by the Sersic profile formula.
+
+        Args:
+            thx (Tensor): The x-coordinate(s) at which to calculate the source brightness. 
+                This could be a single value or a tensor of values.
+            thy (Tensor): The y-coordinate(s) at which to calculate the source brightness. 
+                This could be a single value or a tensor of values.
+            x (dict[str, Any]): A dictionary containing additional parameters.
+
+        Returns:
+            Tensor: The brightness of the source at the given point(s). The output tensor has the same shape as `thx` and `thy`.
+
+        Notes:
+            The Sersic profile is defined as: I(r) = I_e * exp(-k * ((r / r_e)^(1/n) - 1)), 
+            where I_e is the intensity at the effective radius r_e, n is the Sersic index 
+            that describes the concentration of the source, and k is a parameter that 
+            depends on n. In this implementation, we use elliptical coordinates ex and ey, 
+            and the transformation from Cartesian coordinates is handled by `to_elliptical`.
+            The value of k can be calculated in two ways, controlled by `lenstronomy_k_mode`. 
+            If `lenstronomy_k_mode` is True, we use the approximation from Lenstronomy, 
+            otherwise, we use the approximation from Ciotti & Bertin (1999).
+        """
         thx0, thy0, q, phi, index, th_e, I_e = self.unpack(x)
 
         thx, thy = translate_rotate(thx, thy, thx0, thy0, phi)
