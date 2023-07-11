9f8dd4e4ff2429cdbabc9fefa842d05f55e9596e
AlexandreAdam <adam.alexandre01123@gmail.com>
2023-07-11 00:34:06 -0400

    Commented some test to come

--- test/test_pixelated.py
+++ test/test_pixelated.py
@@ -1,10 +1,11 @@
-from caustic import Pixelated
-from caustic.utils import get_meshgrid
+# from caustic import Pixelated
+# from caustic.utils import get_meshgrid
 
 
+# TODO
 # def test_2d_pixelated_brightness():
 
-
+# TODO
 # def test_3d_pixelated_brightness():
 
 
