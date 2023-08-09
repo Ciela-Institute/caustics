d9ca9926e3bef5cf2c4103bd4c1b2e726568fe19
Connor Stone <connorstone628@gmail.com>
2023-08-09 15:30:51 -0400

    change to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,33 @@
-import setuptools
+import setuptools, find_packages
+import caustic.__init__ as caustic
 
-setuptools.setup()
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def read_lines(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read_lines()
+
+setuptools.setup(
+    name = "caustic",
+    version=caustic.__version__,
+    description="A gravitational lensing simulator for the future",
+    long_description=read("README.md"),
+    long_description_content_type='text/markdown',
+    url="https://github.com/Ciela-Institute/caustic",
+    author=caustic.__author__,
+    license="MIT license",
+    packages=find_packages(),
+    install_requires=read_lines("requirements.txt"),
+    keywords = [
+        "gravitational lensing",
+        "astrophysics",
+        "differentiable programming",
+        "pytorch",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",  
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],    
+)
