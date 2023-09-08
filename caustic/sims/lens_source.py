3b5a9f69aeedb8f361307f2dd4929a63ecb13e75
Connor Stone <connorstone628@gmail.com>
2023-09-08 16:23:58 -0400

    sampling grid now defined internally to simulator

--- caustic/sims/lens_source.py
+++ caustic/sims/lens_source.py
@@ -1,31 +1,43 @@
 from ..simulator import Simulator
 from torch.nn.functional import avg_pool2d
+from typing import Tuple
+import torch
 
 __all__ = ("Lens_Source",)
 
 class Lens_Source(Simulator):
     """Lens image of a source.
     
-    Striaghtforward simulator to sample a lensed image of a source object.
+    Striaghtforward simulator to sample a lensed image of a source
+    object. Constructs a sampling grid internally based on the
+    pixelscale and gridding parameters. It can automatically upscale
+    and fine sample an image. This is the most straightforward
+    simulator to view the image if you aready have a lens and source
+    chosen.
 
     Example usage::
 
        import matplotlib.pyplot as plt
-       import caustic, torch
+       import caustic
+
+       cosmo = caustic.FlatLambdaCDM()
+       lens = caustic.lenses.SIS(cosmology = cosmo, x0 = 0., y0 = 0., th_ein = 1.)
+       source = caustic.sources.Sersic(x0 = 0., y0 = 0., q = 0.5, phi = 0.4, n = 2., Re = 1., Ie = 1.)
+       sim = caustic.sims.Lens_Source(lens, source, pixelscale = 0.05, gridx = 100, gridy = 100, upsample_factor = 2)
        
-       lens = caustic.demo.SIS()
-       source = caustic.demo.Sersic()
-       sim = caustic.sims.Lens_Source(lens, source)
-       
-       thx, thy = caustic.demo.grid()
-       img = sim([torch.tensor(1.)], thx, thy, 2)
+       img = sim([1.])
        plt.imshow(img, origin = "lower")
        plt.show()
+
     """
     def __init__(
         self,
         lens,
         src,
+        pixelscale: float = 0.05,
+        gridx: int = 100,
+        gridy: int = 100,
+        upsample_factor: int = 1,
         z_s = None,
         name: str = "sim",
     ):
@@ -36,11 +48,17 @@ class Lens_Source(Simulator):
         self.src = src
         
         self.add_param("z_s", z_s)
+
+        self.upsample_factor = upsample_factor
+        tx = torch.linspace(-0.5 * (pixelscale * gridx), 0.5 * (pixelscale * gridx), gridx*upsample_factor)
+        ty = torch.linspace(-0.5 * (pixelscale * gridy), 0.5 * (pixelscale * gridy), gridy*upsample_factor)
+        self.grid = torch.meshgrid(tx, ty, indexing = "xy")
+
         
-    def forward(self, params, thx, thy, upsample_factor = 1):
+    def forward(self, params):
         z_s, = self.unpack(params)
         
-        bx, by = self.lens.raytrace(thx, thy, z_s, params)
+        bx, by = self.lens.raytrace(*self.grid, z_s, params)
         mu_fine = self.src.brightness(bx, by, params)
         
-        return avg_pool2d(mu_fine.squeeze()[None, None], upsample_factor)[0, 0]
+        return avg_pool2d(mu_fine[None, None], self.upsample_factor, divisor_override = 1).squeeze()
