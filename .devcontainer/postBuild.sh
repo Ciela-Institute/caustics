59d4abfff0824576d652afb288f0a7aa7d7c8e41
Don Setiawan <landungs@uw.edu>
2023-12-13 13:10:39 -0800

    chore(devcontainer): Fix codespaces configuration (#43)

--- .devcontainer/postBuild.sh
+++ .devcontainer/postBuild.sh
@@ -1,5 +1,7 @@
 # For writing commands that will be executed after the container is created
 
+# Installs dev dependencies
+${NB_PYTHON_PREFIX}/bin/pip install nox pre-commit jupyter-book
+
 # Installs `caustics` as local library without resolving dependencies (--no-deps)
-python3 -m pip install -e /workspaces/caustics --no-deps
-python3 -m pip install -e ".[dev]"
+${NB_PYTHON_PREFIX}/bin/pip install --no-deps -e ".[dev]"
