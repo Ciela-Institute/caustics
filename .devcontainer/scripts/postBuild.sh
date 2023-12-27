65e056cf2f0f18f878284ed45269793ec8ae28ad
Don Setiawan <landungs@uw.edu>
2023-12-27 13:58:36 -0800

    fix: Updated devcontainer to simple CPU only (#50)
    * fix: Updated devcontainer to have options
    
    * fix: Remove cuda option for now... doesn't really work

--- .devcontainer/scripts/postBuild.sh
+++ .devcontainer/scripts/postBuild.sh
@@ -1,7 +1,7 @@
 # For writing commands that will be executed after the container is created
 
 # Installs dev dependencies
-${NB_PYTHON_PREFIX}/bin/pip install nox pre-commit jupyter-book
+pip install nox pre-commit jupyter-book
 
 # Installs `caustics` as local library without resolving dependencies (--no-deps)
-${NB_PYTHON_PREFIX}/bin/pip install --no-deps -e ".[dev]"
+pip install -e ".[dev]"
