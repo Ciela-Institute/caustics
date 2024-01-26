7f64e4ef4da28b8c50a1ed78e3cd96aeaa669dea
Landung 'Don' Setiawan <landungs@uw.edu>
2024-01-25 16:19:27 -0800

    test: Add default values for EPL and Sersic for testing
    Added default values for EPL and Sersic objects for performing
    'load_state_dict' testing.

--- tests/sims/conftest.py
+++ tests/sims/conftest.py
@@ -7,13 +7,32 @@ from caustics.cosmology import FlatLambdaCDM
 
 
 @pytest.fixture
-def simple_common_sim():
+def test_epl_values():
+    return {
+        "z_l": 0.5,
+        "phi": 0.0,
+        "b": 1.0,
+        "t": 1.0,
+    }
+
+
+@pytest.fixture
+def test_sersic_values():
+    return {
+        "q": 0.9,
+        "phi": 0.3,
+        "n": 1.0,
+    }
+
+
+@pytest.fixture
+def simple_common_sim(test_epl_values, test_sersic_values):
     class Sim(Simulator):
         def __init__(self):
             super().__init__()
             self.cosmo = FlatLambdaCDM(h0=None)
-            self.epl = EPL(self.cosmo)
-            self.sersic = Sersic()
+            self.epl = EPL(self.cosmo, **test_epl_values)
+            self.sersic = Sersic(**test_sersic_values)
             self.add_param("z_s", 1.0)
 
     sim = Sim()
--- tests/sims/test_simulator.py
+++ tests/sims/test_simulator.py
@@ -51,16 +51,29 @@ class TestSimulator:
         reason="Built-in open has different behavior on Windows",
     )
     def test_load_state_dict(self, simple_common_sim):
+        simple_common_sim.epl.x0 = 0.0
+        simple_common_sim.sersic.x0 = 1.0
+
         fpath = simple_common_sim.state_dict().save()
         loaded_state_dict = StateDict.load(fpath)
 
         # Change a value in the simulator
         simple_common_sim.z_s = 3.0
+        simple_common_sim.epl.x0 = None
+        simple_common_sim.sersic.x0 = None
 
         # Ensure that the simulator has been changed
         assert (
             loaded_state_dict[f"{simple_common_sim.name}.z_s"]
             != simple_common_sim.z_s.value
+        )
+        assert (
+            loaded_state_dict[f"{simple_common_sim.epl.name}.x0"]
+            != simple_common_sim.epl.x0.value
+        )
+        assert (
+            loaded_state_dict[f"{simple_common_sim.sersic.name}.x0"]
+            != simple_common_sim.sersic.x0.value
         )
 
         # Load the state dict form file
@@ -71,6 +84,9 @@ class TestSimulator:
             loaded_state_dict[f"{simple_common_sim.name}.z_s"]
             == simple_common_sim.z_s.value
         )
+
+        assert simple_common_sim.epl.x0.value == torch.as_tensor(0.0)
+        assert simple_common_sim.sersic.x0.value == torch.as_tensor(1.0)
 
         # Cleanup after
         Path(fpath).unlink()
