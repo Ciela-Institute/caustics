71562df27a2832721d678fe6cc421e71dfe01f45
Landung 'Don' Setiawan <landungs@uw.edu>
2024-02-02 13:51:10 -0800

    refactor: Moved cosmology module to separate directory
    Moved the 'cosmology' module that is original one file
    to its own directory.

---
+++ src/caustics/cosmology/__init__.py
@@ -0,0 +1,10 @@
+from .base import Cosmology, h0_default, critical_density_0_default, Om0_default
+from .FlatLambdaCDM import FlatLambdaCDM
+
+__all__ = [
+    "Cosmology",
+    "FlatLambdaCDM",
+    "h0_default",
+    "critical_density_0_default",
+    "Om0_default",
+]
