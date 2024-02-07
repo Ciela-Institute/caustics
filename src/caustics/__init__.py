6bab1040e607b466b987bbb054a0df21c85f131f
AlexandreAdam <adam.alexandre01123@gmail.com>
2024-02-07 12:13:03 -0500

    Updated numba version

--- src/caustics/__init__.py
+++ src/caustics/__init__.py
@@ -1,12 +1,12 @@
 from ._version import version as VERSION  # noqa
 
-from .tests import test
 from .cosmology import *
 from .lenses import *
 from .light import *
 from .data import *
 from .utils import *
 from .sims import *
+from .tests import test
 
 __version__ = VERSION
 __author__ = "Ciela"
