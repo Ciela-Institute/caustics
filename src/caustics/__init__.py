6a2793158724b22c0a88714216927706cd4d9d6e
Connor Stone <connorstone628@gmail.com>
2024-02-13 21:23:48 -0500

    make explicit imports

--- src/caustics/__init__.py
+++ src/caustics/__init__.py
@@ -1,11 +1,95 @@
 from ._version import version as VERSION  # noqa
 
-from .cosmology import *
-from .lenses import *
-from .light import *
-from .data import *
-from .utils import *
-from .sims import *
+from .cosmology import (
+    Cosmology,
+    FlatLambdaCDM,
+    h0_default,
+    critical_density_0_default,
+    Om0_default,
+)
+from .lenses import (
+    ThinLens,
+    ThickLens,
+    EPL,
+    ExternalShear,
+    PixelatedConvergence,
+    Multiplane,
+    NFW,
+    Point,
+    PseudoJaffe,
+    SIE,
+    SIS,
+    SinglePlane,
+    MassSheet,
+    TNFW,
+)
+from .light import Source, Pixelated, Sersic  # PROBESDataset conflicts with .data
+from .data import HDF5Dataset, IllustrisKappaDataset, PROBESDataset
+from .utils import (
+    flip_axis_ratio,
+    translate_rotate,
+    derotate,
+    to_elliptical,
+    get_meshgrid,
+    gaussian_quadrature_grid,
+    gaussian_quadrature_integrator,
+    quad,
+    safe_divide,
+    safe_log,
+    interp1d,
+    interp2d,
+    vmap_n,
+    get_cluster_means,
+    batch_lm,
+    gaussian,
+)
+from .sims import Lens_Source, Simulator
 
 __version__ = VERSION
 __author__ = "Ciela"
+
+__all__ = [
+    "Cosmology",
+    "FlatLambdaCDM",
+    "h0_default",
+    "critical_density_0_default",
+    "Om0_default",
+    "ThinLens",
+    "ThickLens",
+    "EPL",
+    "ExternalShear",
+    "PixelatedConvergence",
+    "Multiplane",
+    "NFW",
+    "Point",
+    "PseudoJaffe",
+    "SIE",
+    "SIS",
+    "SinglePlane",
+    "MassSheet",
+    "TNFW",
+    "Source",
+    "Pixelated",
+    "Sersic",
+    "HDF5Dataset",
+    "IllustrisKappaDataset",
+    "PROBESDataset",
+    "flip_axis_ratio",
+    "translate_rotate",
+    "derotate",
+    "to_elliptical",
+    "get_meshgrid",
+    "gaussian_quadrature_grid",
+    "gaussian_quadrature_integrator",
+    "quad",
+    "safe_divide",
+    "safe_log",
+    "interp1d",
+    "interp2d",
+    "vmap_n",
+    "get_cluster_means",
+    "batch_lm",
+    "gaussian",
+    "Lens_Source",
+    "Simulator",
+]
